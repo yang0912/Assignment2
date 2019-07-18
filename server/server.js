0e19ee84b08929cbfc642981844685dfbd68bbd8
zhuo yang <zhuoyang0912@gmail.com>
2019-07-18 16:29:50 -0700

    deploy

--- server/server.js
+++ server/server.js
@@ -64,14 +64,14 @@ app.get('/', (req, res) => {
 })
 
 //Serve static assets if in production
-if(process.env.NODE_ENV === 'production'){
-    //set static folder
-    app.use(express.static('client/build'));
+// if(process.env.NODE_ENV === 'production'){
+//     //set static folder
+//     app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });
+// }
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Example app listening on port ' + (process.env.PORT || 3000))
